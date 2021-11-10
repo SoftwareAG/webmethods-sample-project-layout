3c3ecc88c6bc60d3f33dd56b2d1aee31a6375326
mohsa <mohsa@SAG-43QT9Y2>
2021-11-10 16:53:23 +0530

    AIA Demo 8 initial commit

---
+++ assets/IS/Packages/AIADemo8/code/source/AIADemo8.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class AIADemo8
+
+{
+	// ---( internal utility methods )---
+
+	final static AIADemo8 _instance = new AIADemo8();
+
+	static AIADemo8 _newInstance() { return new AIADemo8(); }
+
+	static AIADemo8 _cast(Object o) { return (AIADemo8)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void aiademoService (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(aiademoService)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
