76b11872b8023e08186ff53d451c9ee34ceac029
mohsa <mohsa@SAG-43QT9Y2>
2021-11-11 09:05:06 +0530

    Repo Demo5 initial commit

---
+++ assets/IS/Packages/RepoDemo5/code/source/RepoDemo5.java
@@ -0,0 +1,38 @@
+
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class RepoDemo5
+
+{
+	// ---( internal utility methods )---
+
+	final static RepoDemo5 _instance = new RepoDemo5();
+
+	static RepoDemo5 _newInstance() { return new RepoDemo5(); }
+
+	static RepoDemo5 _cast(Object o) { return (RepoDemo5)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void repoJavaService (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(repoJavaService)>> ---
+		// @sigtype java 3.5
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
